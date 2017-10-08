59f0fe8b48a937a16b67aadc6a6ee132f31cc5e5
mrjasonweaver <indyplanets@gmail.com>
2017-10-07 21:00:58 -0500

    clean up app container

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h2>Showmap</h2>
+      <div className="app">
+        <div className="app-header">       
         </div>
-        <p className="App-intro">
-          A live music map app using the Spotify and Ticketmaster APIs
-        </p>
       </div>
     );
   }
