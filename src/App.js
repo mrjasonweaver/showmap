72414e2035d65b7ed575f70088c0d6a065971521
mrjasonweaver <indyplanets@gmail.com>
2017-10-08 19:08:46 -0500

    Shows structure and mock payload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import shows from './shows';
 
 class App extends Component {
   render() {
---
+++ src/shows.js
@@ -0,0 +1,36 @@
+const shows = [
+    {
+        "show_id": 1,
+        "artist_name": "The Revivalists",
+        "venue_name": "Stubbs",
+        "venue_address": "801 Red River St",
+        "coordinates": [
+        {
+            "latitude": 10.54234523,
+            "longitude": 80.31451343
+        }
+        ],
+        "latest_hit": "http://podcast.newrock1041.fm/kfrr2/4858750.mp3",
+        "date": "2012-04-23T18:25:43.511Z",
+        "time": "2012-04-23T18:25:43.511Z",
+        "is_saved": false
+    },
+    {
+        "show_id": 2,
+        "artist_name": "Bonobo",
+        "venue_name": "The Mohawk",
+        "venue_address": "912 Red River St",
+        "coordinates": [
+        {
+            "latitude": 20.54234523,
+            "longitude": 80.31451343
+        }
+        ],
+        "latest_hit": "http://kutx.org/wp-content/uploads/2017/09/Bonobo-Bambro-Koyo-Ganda-feat.-Innov-Gnawa.mp3",
+        "date": "2012-04-23T18:25:43.511Z",
+        "time": "2012-04-23T18:25:43.511Z",
+        "is_saved": true
+    }
+]
+
+export default shows;
