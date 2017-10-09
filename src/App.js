4e65e3378dd20da259c619dacaab7dc7e1f0662b
mrjasonweaver <indyplanets@gmail.com>
2017-10-09 07:32:47 -0500

    ShowList component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import shows from './shows';
 
-const showlist = shows.map((show) =>
+function ShowList(props) {
+  const shows = props.shows;
+  const showItems = shows.map((show) =>
   <ul>
     <li>{show.artist_name}</li>
     <li>Venue: {show.venue_name}</li>
     <li>Address: {show.venue_address}</li>
   </ul>
 );
+  return (
+    <div>{showItems}</div>
+  );
+}
 
 class App extends Component {
   
@@ -16,8 +22,8 @@ class App extends Component {
     return (
       <div className="app">
         <div className="app-header">
-          { showlist }
         </div>
+        <ShowList shows={shows} />
       </div>
     );
   }
