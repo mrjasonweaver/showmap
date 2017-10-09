cf4d97cb060e3d5cb502e43a140ca286fc14368a
mrjasonweaver <indyplanets@gmail.com>
2017-10-08 19:28:06 -0500

    Render showlist

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import shows from './shows';
 
+const showlist = shows.map((show) =>
+  <ul>
+    <li>{show.artist_name}</li>
+    <li>Venue: {show.venue_name}</li>
+    <li>Address: {show.venue_address}</li>
+  </ul>
+);
+
 class App extends Component {
+  
   render() {
     return (
       <div className="app">
-        <div className="app-header">       
+        <div className="app-header">
+          { showlist }
         </div>
       </div>
     );
