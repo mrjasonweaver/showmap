98c1b9b18dc50adb25035d2efde9f6904a37f911
mrjasonweaver <indyplanets@gmail.com>
2017-10-10 17:00:03 -0500

    More show data display

--- src/ShowList.js
+++ src/ShowList.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
 export default function ShowList(props) {
-    const shows = props.shows;
-    const showItems = shows.map((show) =>
-      <li key={show.show_id}>
-        <ul>
-          <li className={'show-' + show.show_id}>{show.artist_name}</li>
-          <li>Venue: {show.venue_name}</li>
-          <li>Address: {show.venue_address}</li>
-        </ul>
-      </li>
-    );
-    return (
-      <ul>{showItems}</ul>
-    );
-  }
+  const shows = props.shows;
+  const showItems = shows.map((show) =>
+    <li key={show.show_id}>
+      <ul>
+        <li className={'show-' + show.show_id}>{show.artist_name}</li>
+        <li>Venue: {show.venue_name}</li>
+        <li>Address: {show.venue_address}</li>
+        <li>Latitude: {show.coordinates.latitude}</li>
+        <li>Longitude: {show.coordinates.longitude}</li>
+        <li>Is saved: {show.is_saved ? 'Yes' : 'No'}</li>
+      </ul>
+    </li>
+  );
+  return (
+    <ul>{showItems}</ul>
+  );
+}
--- src/shows.js
+++ src/shows.js
@@ -4,12 +4,10 @@ const shows = [
         "artist_name": "The Revivalists",
         "venue_name": "Stubbs",
         "venue_address": "801 Red River St",
-        "coordinates": [
-        {
+        "coordinates": {
             "latitude": 10.54234523,
             "longitude": 80.31451343
-        }
-        ],
+        },
         "latest_hit": "http://podcast.newrock1041.fm/kfrr2/4858750.mp3",
         "date": "2012-04-23T18:25:43.511Z",
         "time": "2012-04-23T18:25:43.511Z",
@@ -20,12 +18,10 @@ const shows = [
         "artist_name": "Bonobo",
         "venue_name": "The Mohawk",
         "venue_address": "912 Red River St",
-        "coordinates": [
-        {
+        "coordinates": {
             "latitude": 20.54234523,
             "longitude": 80.31451343
-        }
-        ],
+        },
         "latest_hit": "http://kutx.org/wp-content/uploads/2017/09/Bonobo-Bambro-Koyo-Ganda-feat.-Innov-Gnawa.mp3",
         "date": "2012-04-23T18:25:43.511Z",
         "time": "2012-04-23T18:25:43.511Z",
