759fdec6c71b75d13edfd6e4e155fbcbc42a495f
mrjasonweaver <indyplanets@gmail.com>
2017-10-17 23:22:43 -0500

    Grooming the data

--- src/shows.js
+++ src/shows.js
@@ -5,8 +5,8 @@ const shows = [
         "venue_name": "Stubbs",
         "venue_address": "801 Red River St",
         "coordinates": {
-            "latitude": 10.54234523,
-            "longitude": 80.31451343
+            "latitude": 30.268479,
+            "longitude": -97.736181
         },
         "latest_hit": "http://podcast.newrock1041.fm/kfrr2/4858750.mp3",
         "date": "2018-04-23T00:00:00.511Z",
@@ -18,11 +18,24 @@ const shows = [
         "venue_name": "The Mohawk",
         "venue_address": "912 Red River St",
         "coordinates": {
-            "latitude": 20.54234523,
-            "longitude": 80.31451343
+            "latitude": 30.270152,
+            "longitude": -97.736036
         },
         "latest_hit": "http://kutx.org/wp-content/uploads/2017/09/Bonobo-Bambro-Koyo-Ganda-feat.-Innov-Gnawa.mp3",
         "date": "2018-06-13T01:25:43.511Z",
+        "is_saved": true
+    },
+    {
+        "show_id": 3,
+        "artist_name": "Washed Out",
+        "venue_name": "Emos",
+        "venue_address": "2015 E Riverside Dr",
+        "coordinates": {
+            "latitude": 30.239579,
+            "longitude": -97.728007
+        },
+        "latest_hit": "http://downloads.pitchforkmedia.com/Washed%20Out%20-%20Belong.mp3",
+        "date": "2017-11-10T01:30:43.511Z",
         "is_saved": true
     }
 ]
