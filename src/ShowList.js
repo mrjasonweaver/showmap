eba1e07cc5f231fdf659b8544aace3087c328173
mrjasonweaver <indyplanets@gmail.com>
2017-10-18 19:21:00 -0500

    moving player

--- src/ShowList.js
+++ src/ShowList.js
@@ -29,15 +29,17 @@ export default function ShowList(props) {
   const shows = props.shows;
   const showItems = shows.map((show) =>
     <li key={show.show_id} className={`show-${show.show_id} saved-${show.is_saved ? 'yes' : 'no'}`}>
-      <audio controls="controls">
-        <source src={show.latest_hit} type="audio/mp3"></source>
-      </audio>
       <ul className="show-info">
         <li>{show.artist_name} at {show.venue_name}</li>
         <li>When: {formattedDateTime(show.date)}</li>
         <li>Address: {show.venue_address}</li>
         <li>Latitude: {show.coordinates.latitude}</li>
         <li>Longitude: {show.coordinates.longitude}</li>
+        <li>      
+          <audio controls="controls">
+            <source src={show.latest_hit} type="audio/mp3"></source>
+          </audio>
+      </li>
       </ul>
     </li>
   );
