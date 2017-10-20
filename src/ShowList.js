bae49ff4d9096346045cf19f2d4e562254dfcc4c
mrjasonweaver <indyplanets@gmail.com>
2017-10-20 12:19:07 -0500

    clean up

--- src/ShowList.js
+++ src/ShowList.js
@@ -7,8 +7,8 @@ const ShowList = (props) => {
     <li key={show.show_id} className={`show-${show.show_id} saved-${show.is_saved ? 'yes' : 'no'}`}>
       <ul className="show-info">
         <li>{show.artist_name} at {show.venue_name}</li>
-        <li>When: {formattedDateTime(show.date)}</li>
-        <li>Address: {show.venue_address}</li>
+        <li>{formattedDateTime(show.date)}</li>
+        <li>{show.venue_address}</li>
         <li>Coordinates: {show.coordinates.latitude}, {show.coordinates.longitude}</li>
         <li>      
           <audio controls="controls">
