b791bddeb9aa36902e9b40abe4182e023e75526b
mrjasonweaver <indyplanets@gmail.com>
2017-10-20 12:17:21 -0500

    Clean up

--- src/ShowList.test.js
+++ src/ShowList.test.js
@@ -4,7 +4,3 @@ import ShowList from './ShowList';
 import { shallow, mount, render } from 'enzyme';
 
 const wrapper = shallow(<ShowList />);
-
-test('two plus two is four', () => {
-    expect(2 + 2).toBe(4);
-});
